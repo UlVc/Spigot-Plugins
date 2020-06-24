746683f2ccb7571e0bb86861fab22dff457fa4c5
Ulrich <ulvc@ciencias.unam.mx>
2020-06-24 13:40:03 -0500

    Add World Guard support

--- Utils/TelekinesisEnchant/src/me/ulrich/telekinesis/events/BlockBreak.java
+++ Utils/TelekinesisEnchant/src/me/ulrich/telekinesis/events/BlockBreak.java
@@ -9,21 +9,29 @@ import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class BlockBreak implements Listener {
-
-	@EventHandler
+	
+	/*private WorldGuardPlugin worldGuardPlugin;
+	
+	public BlockBreak(WorldGuardPlugin worldGuardPlugin) {
+        this.worldGuardPlugin = worldGuardPlugin;
+    }*/
+	
+	@EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void onBlockBreak(BlockBreakEvent event) {
+
 		if (event.getPlayer().getInventory().getItemInHand() == null || 
 				!event.getPlayer().getInventory().getItemInHand().hasItemMeta() ||
 				!event.getPlayer().getInventory().getItemInHand().getItemMeta().getLore().toString().contains("Telekinesis") ||
 				event.getPlayer().getGameMode() == GameMode.CREATIVE || event.getPlayer().getGameMode() == GameMode.SPECTATOR ||
 				event.getPlayer().getInventory().firstEmpty() == -1) //No room in their inventory (No need to send the block to their inventory)
 			return;
-		
+
 		if (!event.isCancelled()) { // World Guard protection
 			Player player = event.getPlayer();
 			Block block = event.getBlock();
